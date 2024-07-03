f27f6bf49b01db6a86c90ce27c4dcec6f18032d1
Gshii <117040064+Gshii@users.noreply.github.com>
2024-07-03 17:37:11 +0300

    Initial commit

---
+++ ExspenseTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExspenseTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
